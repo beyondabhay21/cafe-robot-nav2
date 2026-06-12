3daa8e33ef75f4c9973ed997d8886d2fed81963f
beyondabhay21 <abhaypotdar06@gmail.com>
2026-06-12 13:43:22 +0530

    Expanded delivery state machine

--- src/cafe_robot/cafe_robot/robot_states.py
+++ src/cafe_robot/cafe_robot/robot_states.py
@@ -1,12 +1,19 @@
 from enum import Enum
 
-
 class RobotState(Enum):
 
     IDLE = "idle"
 
     TO_KITCHEN = "to_kitchen"
 
+    WAIT_KITCHEN = "wait_kitchen"
+
     TO_TABLE = "to_table"
 
+    WAIT_TABLE = "wait_table"
+
+    BACK_KITCHEN = "back_kitchen"
+
     BACK_HOME = "back_home"
+
+    CANCELLED = "cancelled"
